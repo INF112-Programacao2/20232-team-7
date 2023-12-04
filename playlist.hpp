6d743695cc65a6f365ccafd0b8d8df11f769a3e6
Laís Sales <laisrsales@outlook.com>
2023-12-04 14:39:00 -0300

    Arquivos .hpp de playlist e perfil

---
+++ playlist.hpp
@@ -0,0 +1,26 @@
+#ifndef PLAYLIST_HPP_
+#define PLAYLIST_HPP_
+#include <string>
+#include <vector>
+#include "musica.h"
+
+class Playlist {
+    private:
+        std::string _nome;
+        std::vector<Musica*> _musicas;
+        bool _visibilidade;
+    public:
+        Playlist(std::string nome);
+        ~Playlist();
+
+        //GETTERS
+        std::string get_nome();
+        bool get_visibilidade();
+        int get_quant_musicas();
+        Musica* get_musica(int posicao);
+        void adicionar_musica(Musica* musica);
+        void apagar_musica(int posicao);
+
+};
+
+#endif
